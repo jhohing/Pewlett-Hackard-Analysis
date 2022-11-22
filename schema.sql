dc52eeaa6bfe293ff319b4efde81ba384e9591ff
jhohing <jnh0627@yahoo.com>
2022-11-21 23:08:44 -0500

    Updating schema.sql file

--- schema.sql
+++ schema.sql
@@ -38,5 +38,14 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   PRIMARY KEY (emp_no)
 );*/
 
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
 select *
  from departments
